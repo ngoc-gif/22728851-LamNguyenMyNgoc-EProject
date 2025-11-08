41f02e47d95b616ed90390f22fb2d4d527afaecd
lamngoc <lamngoc562004@gmail.com>
2025-11-08 11:33:24 +0700

    cicd

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -99,7 +99,7 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-// tim san pham theo id
+    // tim sp
     async getID(req,res,next){
       const products = await Product.findById(req.params.id);
       res.status(200).json(products);
